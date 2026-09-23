14abbce5343727ed99acb69a333cb6ca6183a0f2
Alan Ibarra <alan.ibarra02@utrgv.edu>
2026-09-23 17:46:20 -0500

    small improvement

--- js/example.js
+++ js/example.js
@@ -1,8 +1,14 @@
 $(function() {
   
-  // TODO: Create a list of JS Object each representing a game. 
-  // Use the data contained in the <ul> to build this list of game titles.
-
+  // Create a list of JavaScript objects.
+  // Each object represents a game.
+var games = [
+  { title: 'The Legend of Zelda: Breath of the Wild' },
+  { title: 'God of War Ragnarök' },
+  { title: 'Halo Infinite' },
+  { title: 'Minecraft' },
+  { title: 'Super Mario Odyssey' }
+];
 
   var gameList, newItemForm, newItemButton;
   var item = '';                                 
@@ -12,36 +18,51 @@ $(function() {
   newItemForm = $('#newItemForm');              
   newItemButton = $('#newItemButton');          
 
-  // TODO:  Render game titles as list items inside the <ul>. 
-  // To do so, create a function that loops through each object in the game list, 
-  // create a new node "list item" holding the game title and 
-  // inject the new node inside the <ul>.
+  // Render game titles as list items inside the <ul>.
+  function renderGames() {
+    gameList.empty();
+
+    games.forEach(function(game) {
+      var newItem = $('<li></li>');
+      newItem.text(game.title);
+      gameList.append(newItem);
+    });
+  }
+
+  // Display the games when the page loads.
+  renderGames();
 
 
-
+  // Update the number of games displayed.
   function updateCount() {                      
-    var items = $('li').length; 
+    var items = $('#gameList li').length;
     $('#counter').text(`${items}`);                   
   }
+
   updateCount();                                 
 
   
+  // Show/hide the new game form.
   newItemButton.show();                         
   newItemForm.hide();                           
+
   $('#showForm').on('click', function() {        
     newItemButton.hide();                       
     newItemForm.show();                         
   });
 
   
+  // Add a new game to the list.
   newItemForm.submit(function(e) {       
     e.preventDefault();                         
+
     var text = $('input:text').val();           
     gameList.append(`<li>${text}</li>`);      
+
     $('input:text').val('');                    
     updateCount();                              
   });  
 
-
-
 });
+
+
